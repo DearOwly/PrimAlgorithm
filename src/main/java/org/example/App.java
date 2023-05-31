32934763873a88e0fefb3dfc60ca3f44f27e0fe7
DearOwly <mydearshinchan@gmail.com>
2023-05-31 12:24:33 +0300

    add comments about algorithm

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,29 +4,39 @@ import java.util.PriorityQueue;
 
 public class App
 {
-    public static void primMST(int[][] graph) {
+    public static void primMST(int[][] graph) { //реализация алгоритма
         int V = graph.length;
-        int[] parent = new int[V];
-        int[] key = new int[V];
-        boolean[] mstSet = new boolean[V];
+        int[] parent = new int[V]; //массив хранения родительских вершин
+        int[] key = new int[V]; //массив хранения веса ребер
+        boolean[] mstSet = new boolean[V]; //массив флагов посещения
 
+        //концептуально просто заполняем массивы длины ребер и посещений
+        //используем следующие принципы
+        //длины ребер ставим неадекватно большими
+        //все вершины объявляем непосещенными
         for (int i = 0; i < V; i++) {
             key[i] = Integer.MAX_VALUE;
             mstSet[i] = false;
         }
 
+        //стартуем с 0 вершиины, говорим, что предка у нее нет
         key[0] = 0;
         parent[0] = -1;
 
+        //приоритетная очередь, принимающая вершину и ее ключ
         PriorityQueue<Node> pq = new PriorityQueue<>(V, new Node());
 
+        //добавляем нулевую (она же страртовая) вершину
         pq.add(new Node(0, key[0]));
 
-        while (!pq.isEmpty()) {
-            int u = pq.poll().vertex;
 
+        while (!pq.isEmpty()) { //пока мы не очистили очередь
+            //удаляем с возвращением нынешний элемент
+            int u = pq.poll().vertex;
+            //ставим метку, что мы ее посетили
             mstSet[u] = true;
 
+            //запускаем цикл для выбора вершины, в которую мы отправимся далее
             for (int v = 0; v < V; v++) {
                 if (graph[u][v] != 0 && !mstSet[v] && graph[u][v] < key[v]) {
                     parent[v] = u;
@@ -39,6 +49,7 @@ public class App
         printMST(parent, graph);
     }
 
+    //простой вывод остова
     public static void printMST(int[] parent, int[][] graph) {
         System.out.println("Edge \tWeight");
         for (int i = 1; i < graph.length; i++) {
